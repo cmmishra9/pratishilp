068cbda116c822a19cbe013407d3780a3804d9e7
Harsh Vardhan Singh <harsh.17vardhan@gmail.com>
2021-04-23 12:19:36 +0530

    home view added

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://pratishilp.borg/';
+$config['base_url'] = 'http://pratishilp.local/';
 
 /*
 |--------------------------------------------------------------------------

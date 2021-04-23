749c0a05946351995cf393ffdd29cd3b48aec068
vaishalisingh6698 <43922978+vaishalisingh6698@users.noreply.github.com>
2021-04-23 11:20:39 +0530

    My front end Controller added manually

---
+++ application/core/MY_frontVontroller.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') ON exit('No direct script access');
+
+class MY_frontController extends CI_Controller
+{
+    public function __construct()
+    {
+
+    }
+    public function index()
+    {
+        
+    }
+}
